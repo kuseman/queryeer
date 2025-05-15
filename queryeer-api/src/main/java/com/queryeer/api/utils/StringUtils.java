577d820f7327484ac6c0f0b1c02409346e8471bf
Linus Brimstedt <linus.brimstedt@ellos.se>
2025-05-15 21:28:39 +0200

    fix: different filter algorithm for quick search in dbs

--- queryeer-api/src/main/java/com/queryeer/api/utils/StringUtils.java
+++ queryeer-api/src/main/java/com/queryeer/api/utils/StringUtils.java
@@ -24,4 +24,68 @@ public class StringUtils
         }
         return true;
     }
+
+    /**
+     * Finds needle in haystack, regardless of case and order of characters. At least two
+     * characters in a row must be found in the same order as in needle.s
+     *
+     * @param needle the string to find
+     * @param haystack the string to search in
+     * @return true if needle is found in haystack
+     */
+    public static boolean findStringsInString(String needle, String haystack)
+    {
+        if (StringUtils.isBlank(haystack))
+        {
+            return false;
+        }
+
+        if (StringUtils.isBlank(needle))
+        {
+            return true;
+        }
+
+        String lowerNeedle = needle.toLowerCase();
+        String lowerHaystack = haystack.toLowerCase();
+
+        if (lowerHaystack.contains(lowerNeedle))
+        {
+            return true;
+        }
+
+        char[] lowerNeedleUnique = lowerNeedle.toCharArray();
+        char[] lowerHaystackUnique = lowerHaystack.toCharArray();
+
+        int haystackIndex = 0;
+        int needleIndex = 0;
+        int charsInRow = 0;
+        while (haystackIndex < lowerHaystackUnique.length
+                && needleIndex < lowerNeedleUnique.length)
+        {
+            while (haystackIndex < lowerHaystackUnique.length
+                    && needleIndex < lowerNeedleUnique.length
+                    && lowerHaystackUnique[haystackIndex] == lowerNeedleUnique[needleIndex]
+                    && (charsInRow > 0
+                            || lowerHaystackUnique[haystackIndex + 1] == lowerNeedleUnique[needleIndex + 1]))
+            {
+                charsInRow++;
+                lowerHaystackUnique[haystackIndex] = '*';
+                haystackIndex++;
+                needleIndex++;
+            }
+            if (needleIndex == lowerNeedleUnique.length)
+            {
+                return true;
+            }
+            haystackIndex++;
+
+            if (charsInRow > 2
+                    && haystackIndex >= lowerHaystackUnique.length)
+            {
+                charsInRow = 0;
+                haystackIndex = 0;
+            }
+        }
+        return false;
+    }
 }
