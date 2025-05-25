f04c03687974190ad6d1edb05c3135d011bc7f33
Brimstedt <linus@brimstedt.se>
2025-05-25 19:53:18 +0200

    feat: Filtering in DialogUtils allows several partial matches (#51)

--- queryeer-api/src/main/java/com/queryeer/api/utils/StringUtils.java
+++ queryeer-api/src/main/java/com/queryeer/api/utils/StringUtils.java
@@ -24,4 +24,71 @@ public class StringUtils
         }
         return true;
     }
+
+    /**
+     * Finds needle in haystack, regardless of case and order of characters. At least two characters in a row must be found in the same order as in needle.
+     *
+     * @param needle the string to find
+     * @param haystack the string to search in
+     * @return true if needle is found in haystack
+     */
+    public static boolean findStringsInString(String needle, String haystack)
+    {
+        if (StringUtils.isBlank(haystack))
+        {
+            return false;
+        }
+
+        if (StringUtils.isBlank(needle))
+        {
+            return true;
+        }
+
+        String lowerNeedle = needle.toLowerCase()
+                .replace(" ", "");
+        String lowerHaystack = haystack.toLowerCase();
+
+        if (lowerHaystack.contains(lowerNeedle))
+        {
+            return true;
+        }
+
+        char[] lowerNeedleUnique = lowerNeedle.toCharArray();
+        char[] lowerHaystackUnique = lowerHaystack.toCharArray();
+
+        int haystackIndex = 0;
+        int needleIndex = 0;
+        int charsInRow = 0;
+        while (haystackIndex < lowerHaystackUnique.length
+                && needleIndex < lowerNeedleUnique.length)
+        {
+            while (haystackIndex < lowerHaystackUnique.length
+                    && needleIndex < lowerNeedleUnique.length // didnt reach end of either string
+                    && lowerHaystackUnique[haystackIndex] == lowerNeedleUnique[needleIndex] // this char matches
+                    && (charsInRow > 0 // already more than one char in a row
+                            || needleIndex >= lowerNeedleUnique.length - 1 // Only one char left of needle
+                            || (haystackIndex + 1 < lowerHaystackUnique.length // next char also matches
+                                    && needleIndex + 1 < lowerNeedleUnique.length
+                                    && lowerHaystackUnique[haystackIndex + 1] == lowerNeedleUnique[needleIndex + 1])))
+            {
+                charsInRow++;
+                lowerHaystackUnique[haystackIndex] = '*';
+                haystackIndex++;
+                needleIndex++;
+            }
+            if (needleIndex == lowerNeedleUnique.length)
+            {
+                return true;
+            }
+            haystackIndex++;
+
+            if (charsInRow > 2
+                    && haystackIndex >= lowerHaystackUnique.length)
+            {
+                charsInRow = 0;
+                haystackIndex = 0;
+            }
+        }
+        return false;
+    }
 }
--- queryeer-ide/src/test/java/com/queryeer/UtilsTest.java
+++ queryeer-ide/src/test/java/com/queryeer/UtilsTest.java
@@ -5,9 +5,12 @@ import static java.util.Arrays.asList;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.queryeer.api.utils.StringUtils;
 
 /** Test of {@link PluginHandler} */
 public class UtilsTest extends Assert
@@ -19,5 +22,60 @@ public class UtilsTest extends Assert
         plugins.sort((a, b) -> Utils.compareVersions(a.getName(), b.getName()));
 
         assertEquals(asList(new File("pluginA-2"), new File("pluginA-0.11"), new File("pluginA-0.10.0"), new File("pluginA-0.2.1"), new File("pluginB")), plugins);
+    }
+
+    @Test
+    public void test_when_needle_is_not_in_haystack_then_return_false()
+    {
+        assertFalse(StringUtils.findStringsInString("", ""));
+        assertFalse(StringUtils.findStringsInString("abc1mno", "abcdefghijklmnop"));
+        assertFalse(StringUtils.findStringsInString("checkoutprod", "EComCustomerMasterProd"));
+        assertFalse(StringUtils.findStringsInString("ee", "abcdefghijklmnop"));
+        assertFalse(StringUtils.findStringsInString("q", "abcdefghijklmnop"));
+        assertFalse(StringUtils.findStringsInString("sadf", ""));
+        assertFalse(StringUtils.findStringsInString("xxx", "abcdefghijklmnop"));
+        assertFalse(StringUtils.findStringsInString("za", "abcdefghijklmnop"));
+        assertFalse(StringUtils.findStringsInString("acb", "abcdefghijklmnop"));
+        assertFalse(StringUtils.findStringsInString("opab", "abcdefghijklmnop"));
+        assertFalse(StringUtils.findStringsInString("customemasttest", "SupplyChainTest - Monitoring -BUS_CustomersServiceTest"));
+        assertFalse(StringUtils.findStringsInString("customemasttest", "SupplyChainTest - Monitoring -BUS_CustomersValidationTest"));
+    }
+
+    @Test
+    public void test_when_needle_is_in_haystack_then_return_true()
+    {
+        assertTrue(StringUtils.findStringsInString("cud", "EComCustomerMasterProd"));
+        assertTrue(StringUtils.findStringsInString("cust pr t", "EComCustomerMasterProd"));
+        assertTrue(StringUtils.findStringsInString("cust pr maste d", "EComCustomerMasterProd"));
+        assertTrue(StringUtils.findStringsInString("cust prod mast", "EComCustomerMasterProd"));
+        assertTrue(StringUtils.findStringsInString("custprodmast", "EComCustomerMasterProd"));
+        assertTrue(StringUtils.findStringsInString("custprod", "EComCustomerMasterProd"));
+
+        assertTrue(StringUtils.findStringsInString("efgbcd", "abcdefghijklmnop"));
+        assertTrue(StringUtils.findStringsInString("custprodmast", "EComCustomerMasterProd"));
+        assertTrue(StringUtils.findStringsInString("custprod", "EComCustomerMasterProd"));
+        assertTrue(StringUtils.findStringsInString("abcmno", "abcdefghijklmnop"));
+        assertTrue(StringUtils.findStringsInString("", "abcdefghijklmnop"));
+        assertTrue(StringUtils.findStringsInString("abc", "abcdefghijklmnop"));
+        assertTrue(StringUtils.findStringsInString("DeF", "abcdefghijklmnop"));
+        assertTrue(StringUtils.findStringsInString("EFG", "abcdefghijklmnop"));
+        assertTrue(StringUtils.findStringsInString("efgcd", "abcdefghijklmnop"));
+    }
+
+    @Test
+    public void test_when_100000_searches_are_performed_be_quick_about_it()
+    {
+        long startTime = System.currentTimeMillis();
+        Random r = new Random();
+        String haystack = "Some string To Test Performance with";
+        String needle = "needleschmeedle";
+        for (int i = 0; i < 100000; i++)
+        {
+            needle = (needle + (char) (r.nextInt(26) + 'a')).substring(0, 10);
+            haystack = (haystack + (char) (r.nextInt(26) + 'a')).substring(0, 20);
+            StringUtils.findStringsInString(needle, haystack);
+        }
+        long duration = (System.currentTimeMillis() - startTime);
+        assertFalse("Performance test took too long: " + duration + "ms", duration > 1000);
     }
 }
